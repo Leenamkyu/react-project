fd5c68144d1c930b57e6c4ccfa552e552bc585ab
Leenamkyu <dlskarb032@naver.com>
2019-12-31 04:27:12 +0900

    ver 1

---
+++ reactfrontend/src/Components/PostView.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const dummy_prop = {
+    title: '테스트용 타이틀입니다',
+    content: '테스트용 글입니다'
+}
+
+
+class PostView extends Component {
+    render() {
+        const {id, title, content} = this.props
+        return (
+            <div>
+                {id}
+                <h3>{title}</h3>
+                <p>{content}</p>
+            </div>
+        )
+    }
+}
+
+
+export default PostView;
